18674bc43ec2ac8538f3031a848a2bda2c15f623
b535011b <b535011b@mails.cc.ehime-u.ac.jp>
2018-07-08 01:22:52 +0900

    Add fake

---
+++ fake-build/InterBuil/src/cla/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cla
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
